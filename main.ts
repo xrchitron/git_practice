ecddfb1f7a64a5d9443be44074d774506b5d176e
Yuhao <yuhaochen1124@gmail.com>
2024-04-13 16:36:36 +0800

    init: initialize the project

---
+++ main.ts
@@ -0,0 +1,6 @@
+const theStartLine = 1;
+const theSecondLine = 2;
+const theThirdLine = 3;
+const theFourthLine = 4;
+
+console.log("Hello World!");
---
+++ test/test_file.ts
@@ -0,0 +1,16 @@
+// this is the git practice file
+const theTestVar: number = 100;
+const theStringVar: string = "This is a string";
+const theBooleanVar: boolean = true;
+const theArrayVar: number[] = [1, 2, 3, 4];
+
+interface ITheObject {
+  name: string;
+  age: number;
+}
+const theObjectVar: ITheObject = { name: "John", age: 30 };
+
+interface ITheFunction {
+  (a: number, b: number): number;
+}
+const theFunctionVar: ITheFunction = (a, b) => a + b;
