fd1fa5939723a61010a0343dd60e5080795171f5
Yuhao <yuhaochen1124@gmail.com>
2024-04-14 17:06:04 +0800

    chore: test third branch

--- main.ts
+++ main.ts
@@ -3,5 +3,12 @@ const theSecondLine = 2;
 const theThirdLine = 3;
 const theFourthLine = 4;
 
-console.log("Hello World!");
 const testComment = "this is the git practice file";
+console.log("Hello World!");
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+const nextNumberVar = 10; // this is the next number
+console.log(nextNumberVar);
